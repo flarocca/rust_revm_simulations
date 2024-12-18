d7401c4e16044190c26971a04141337d63aa3a6d
Facundo La Rocca <facu.larocca@gmail.com>
2024-12-18 10:38:10 -0300

    Cusotm UniswapV3 simulator implemented

---
+++ contracts/src/UniswapV3Simulator.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.14;
+
+import { console } from "forge-std/console.sol";
+import "./HelperLibraries.sol";
+import { IERC20, IUniswapV3Pool } from "./Interfaces.sol";
+
+contract UniswapV3Simulator {
+    uint160 internal constant MIN_SQRT_RATIO = 4295128739;
+    uint160 internal constant MAX_SQRT_RATIO = 1461446703485210103287273052203988822378723970342;
+
+    function swap(
+        address poolAddress,
+        address tokenIn,
+        address tokenOut,
+        bool zeroForOne,
+        uint256 amountIn
+    )
+        external
+        returns (
+            uint256 tokenInBalanceBefore,
+            uint256 tokenInBalanceAfter,
+            uint256 tokenOutBalanceBefore,
+            uint256 tokenOutBalanceAfter
+        )
+    {
+        tokenInBalanceBefore = IERC20(tokenIn).balanceOf(address(this));
+        tokenOutBalanceBefore = IERC20(tokenOut).balanceOf(address(this));
+
+        // We ignore the return values to rely on the real balances anfter swapping.
+        // This is because some tokens may have a fee on transfer, which will affect the balances.
+        internal_swap(address(this), poolAddress, tokenIn, zeroForOne, int256(amountIn));
+
+        tokenInBalanceAfter = IERC20(tokenIn).balanceOf(address(this));
+        tokenOutBalanceAfter = IERC20(tokenOut).balanceOf(address(this));
+    }
+
+    function uniswapV3SwapCallback(int256 amount0Out, int256 amount1Out, bytes calldata data) external {
+        bytes memory dataBytes = data;
+        address tokenOut;
+        assembly {
+            tokenOut := mload(add(dataBytes, 20))
+        }
+
+        if (amount0Out > 0) {
+            IERC20(tokenOut).transfer(msg.sender, uint256(amount0Out));
+        } else if (amount1Out > 0) {
+            IERC20(tokenOut).transfer(msg.sender, uint256(amount1Out));
+        }
+    }
+
+    function internal_swap(address recipient, address poolAddress, address tokenIn, bool zeroForOne, int256 amount)
+        internal
+        returns (int256, int256)
+    {
+        bytes memory addressBytes = abi.encodePacked(tokenIn);
+
+        try IUniswapV3Pool(poolAddress).swap(
+            recipient, zeroForOne, amount, (zeroForOne ? MIN_SQRT_RATIO + 1 : MAX_SQRT_RATIO - 1), addressBytes
+        ) returns (int256 amount0Delta, int256 amount1Delta) {
+            return (amount0Delta, amount1Delta);
+        } catch (bytes memory lowLevelData) {
+            revert(string(abi.encodePacked("UNISWAP_V3 Revert: ", lowLevelData)));
+        }
+    }
+}
---
+++ contracts/test/UniswapV3Simulator.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.14;
+
+import { Test} from "forge-std/Test.sol";
+import { console } from "forge-std/console.sol";
+import { UniswapV3Simulator } from "../src/UniswapV3Simulator.sol";
+import { IERC20 } from "../src/Interfaces.sol";
+
+contract UniswapV3SimulatorTest is Test {
+    IERC20 WETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IERC20 USDT = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+    address POOL = address(0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36);
+    
+    string RPC_URL = "https://eth-mainnet.g.alchemy.com/v2/Sg0Hh6Bcv4Dfj2OcU4_6VePVPED-8-MD";
+    
+    function setUp() public { }
+
+    function test_simple_swap() public {
+        bool zeroForOne = true; // `true` indicates that we want to swap token0 for token1.
+        uint256 amountIn = 1 ether; // Positive indicates we are swapping exact input, negative indicates exact output.
+
+        UniswapV3Simulator simulator = new UniswapV3Simulator();
+
+        vm.selectFork(vm.createFork(RPC_URL, 21424541));
+
+        deal(address(WETH), address(simulator), 10 ether);
+
+        uint256 tokenInBeforeTest = WETH.balanceOf(address(simulator));
+        uint256 tokenOutBeforeTest = USDT.balanceOf(address(simulator));
+
+        (uint256 tokenInBalanceBefore, uint256 tokenInBalanceAfter, uint256 tokenOutBalanceBefore, uint256 tokenOutBalanceAfter) = 
+            simulator.swap(POOL, address(WETH), address(USDT), zeroForOne, amountIn);
+
+        uint256 tokenInAfterTest = WETH.balanceOf(address(simulator));
+        uint256 tokenOutAfterTest = USDT.balanceOf(address(simulator));
+
+        assertEq(tokenInBalanceBefore, tokenInBeforeTest);
+        assertEq(tokenOutBalanceBefore, tokenOutBeforeTest);
+        assertEq(tokenInBalanceAfter, tokenInAfterTest);
+        assertEq(tokenOutBalanceAfter, tokenOutAfterTest);
+
+        assertEq(tokenInBalanceAfter, tokenInBalanceBefore - amountIn);
+    }
+}
